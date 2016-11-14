ab767ad4e67e9e5386d54151f835a4954f98c6ce
Roman Anderson <greenclawanderson@gmail.com>
2016-11-14 22:26:51 +0300

    1. Restructured tables: log, reservation, guest. 2. Added logs generated till 2014 year to 2016.

--- stored_procedures.sql
+++ stored_procedures.sql
@@ -7,13 +7,6 @@ END;
 $$ LANGUAGE plpgsql;
 
 
-create or replace function add_room(num integer, hotel_id integer, type integer)
-returns void as $$
-BEGIN
-	insert into room (num, hotel_id, type) 
-	values (num, hotel_id, type);
-END;
-$$ LANGUAGE plpgsql;
 
 
 create or replace function add_room(num integer, hotel_id integer, type integer)
@@ -23,3 +16,53 @@ BEGIN
 	values (num, hotel_id, type);
 END;
 $$ LANGUAGE plpgsql;
+
+
+
+create or replace function check_reservation(cur_time TIMESTAMP, arrive DATE, dep DATE)
+returns boolean as $$
+DECLARE
+	c_time DATE;
+BEGIN
+	c_time := cur_time::date;
+	if ((c_time <= arrive) or (c_time <= dep)) THEN
+		RAISE EXCEPTION 'Date of arrive and depart must be greater then current!';
+	END IF;
+	RETURN TRUE;
+END
+$$ LANGUAGE plpgsql;
+
+
+
+
+create or replace function reserve_room(room_id integer, guest_id integer, arrive varchar, dep varchar)
+returns boolean as $$
+DECLARE 
+	c_time TIMESTAMP;
+	a DATE;
+	d DATE;
+BEGIN
+	c_time := CURRENT_TIMESTAMP;
+	a = to_date(arrive, 'YYYY-MM-DD');
+	d = to_date(dep, 'YYYY-MM-DD');
+	if (check_reservation(c_time, a, d)) THEN
+		insert into reservation (guest_id , room_id, reserve_time, arrive, departure) 
+		values (room_id, guest_id, c_time, a, d);
+	END IF;
+	RETURN TRUE;
+END;
+$$ LANGUAGE plpgsql;
+
+
+
+
+create or replace function to_archive(num integer, hotel_id integer, type integer)
+returns void as $$
+BEGIN
+	insert into room (num, hotel_id, type) 
+	values (num, hotel_id, type);
+END;
+$$ LANGUAGE plpgsql;
+
+
+
